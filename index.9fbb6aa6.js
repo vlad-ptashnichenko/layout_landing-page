017da13fea4a6c494875180a02c39225d8af138a
Vlad Ptashnichenko <vp.vlad.vp@gmail.com>
2024-02-27 17:07:16 +0200

    make build

---
+++ index.9fbb6aa6.js
@@ -0,0 +1,3 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});//# sourceMappingURL=index.9fbb6aa6.js.map
+
+//# sourceMappingURL=index.9fbb6aa6.js.map
